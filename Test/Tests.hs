5a56a7be5960d580362aecd6871015359b4babce
Marcus Ofenhed <marcus@conditionraise.se>
2018-03-02 20:55:43 +0100

    Fixed the broken test cases.

--- Test/Tests.hs
+++ Test/Tests.hs
@@ -1,4 +1,4 @@
-import Crypto.RandomMonad (runRndT, getRandomM, RndStateList(RndStateListSequencial, RndStateListParallel), getRandomByteStringM)
+import Crypto.RandomMonad (runRndT, getRandomM, RndStateList(..), getRandomByteStringM, RndStateParallelization(..))
 
 import Control.Monad.ST (runST)
 import Test.QuickCheck
@@ -9,8 +9,9 @@ import System.Exit (exitWith, ExitCode(ExitSuccess, ExitFailure))
 prop_parallelEquals1 = do
   let lists = [BiS.bitStringLazy $ LC8.pack $ Prelude.concat $ Prelude.repeat "Hej, jag loopar här",
                BiS.bitStringLazy $ LC8.pack $ Prelude.concat $ Prelude.repeat "Hej, jag loopar också här"]
-      (first, _) = runST $ runRndT (RndStateListSequencial lists) $ getRandomByteStringM 9001
-      (second, _) = runST $ runRndT (RndStateListParallel lists) $ getRandomByteStringM 9001
-   in first == second
+      (first, _) = runST $ runRndT (RndStateList RndStateSeq lists) $ getRandomByteStringM 9001
+      (second, _) = runST $ runRndT (RndStateList RndStateParallel lists) $ getRandomByteStringM 9001
+      (third, _) = runST $ runRndT (RndStateList RndStatePrecalculate lists) $ getRandomByteStringM 9001
+   in first == second && second == third
 
 main = exitWith $ if prop_parallelEquals1 then ExitSuccess else ExitFailure 1
